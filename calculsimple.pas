cd3ca65ece041de99593434be4737f629e98345d
egefisgin <38686980+egefisgin@users.noreply.github.com>
2023-12-07 21:19:32 +0100

    Calcul

--- calculsimple.pas
+++ calculsimple.pas
@@ -11,6 +11,7 @@ function calcul(s: String): Real;
 Implementation
 uses crt,sysutils,math;
 
+
 //////////////////
  procedure lireChaine(var str : string);
 	begin
@@ -27,6 +28,7 @@ Writeln('Quand tu doit faire calcul de cos ou sin, tu dois ecrire c pour cosinus
 Writeln('Example : =s90 ca veut dire sin(90) ');
 Writeln('Les 4 operations mathematique sont memes');
 Writeln('x pour la multiplication, + pour  addition, / pour la division, - pour la soustraction ');
+Writeln('nlx pour la logaritma ,n est la base ,x est la valeur et et l est la logaritma ');
 
 
 
@@ -37,7 +39,7 @@ end;
 //////////////////
 function Parantehese(expression: String ;  openPos : Integer):Integer;
 var j:Integer;
-  // Parantez içindeki ilk işlemi bul
+  // Trouver la calcul dans la parathese
  begin 
   for j := openPos to Length(expression) do
     if expression[j] = ')' then
@@ -58,15 +60,15 @@ var j:Integer;
 	   	while i > 0 do
 		begin
 	    if (s[i] = '(') then
-    begin
-      closePos := Parantehese(s, i + 1); // trouver la Parantehese
-      s1 := Copy(s, i + 1, closePos - i - 1); // prendre entre les paratehes
-      s := Copy(s, 1, i - 1) + FloatToStr(calcul(s1)) + Copy(s, closePos + 1, Length(s)); // calcul 
-      i := Length(s); // Yeni uzunluğu al
-    end
-    else
-      Dec(i);
-  end;
+		 begin
+			closePos := Parantehese(s, i + 1); // trouver la Parantehese
+			s1 := Copy(s, i + 1, closePos - i - 1); // prendre entre les paratehes
+			s := Copy(s, 1, i - 1) + FloatToStr(calcul(s1)) + Copy(s, closePos + 1, Length(s)); // calcul 
+			i := Length(s); // calcul la length 
+		 end
+	    else
+        Dec(i);
+       end;
 
 		i := Length(s);
 		while i > 0 do
@@ -74,7 +76,7 @@ var j:Integer;
 			if (s[i] = '+') or (s[i] = '-') then
 			begin
 				s1 := Copy(s, 0, i - 1);
-				s2 := Copy(s, i + 1, Length(s));
+				s2 := Copy(s, i + 1, Length(s)-(i-1));
 				if s[i] = '+' then
 				calcul := calcul(s1) + calcul(s2)
 				else
@@ -90,7 +92,7 @@ var j:Integer;
 			if (s[i] = 'x') or (s[i] = '/') then
 			begin
 				s1 := Copy(s, 0, i - 1);
-				s2 := Copy(s, i + 1, Length(s));
+				s2 := Copy(s, i + 1, Length(s)-(i-1));
 				if s[i] = 'x' then
 					calcul := calcul(s1) * calcul(s2)
 				else
@@ -103,23 +105,63 @@ var j:Integer;
 	   i := Length(s);
 	   while i > 0 do
 	   begin
-		if (s[i] = '^') then
+		if (s[i] = 'l') then
 		begin
          s1 := Copy(s, 0, i - 1);
-         s2 := Copy(s, i + 1, Length(s));
-		 if s[i] = '^' then
-			calcul := power(calcul(s1),calcul(s2));
+         s2 := Copy(s, i + 1, Length(s)-(i-1));
+			calcul := Logn(calcul(s1),calcul(s2));
+			exit;
+			end
+         else
+		 i:=i-1;
+        end;	
+        
+        i := Length(s);
+	   while i > 0 do
+	   begin
+		if (s[i] = 'r') then
+		begin
+         s1 := Copy(s, 0, i - 1);
+         s2 := Copy(s, i + 1, Length(s)-(i-1));
+			calcul :=  Sqrt(calcul(s2));
 			exit;
 			end
          else
 		 i:=i-1;
         end;	
         i := Length(s);
+	   while i > 0 do
+	   begin
+		if (s[i] = 'e') then
+		begin
+         s1 := Copy(s, 0, i - 1);
+         s2 := Copy(s, i + 1, Length(s)-(i-1));
+			calcul :=  Exp(calcul(s2));
+			exit;
+			end
+         else
+		 i:=i-1;
+        end;	
+       	i := Length(s);
+	     while i > 0 do
+	     begin
+		 if (s[i] = '^') then
+	    	begin
+            s1 := Copy(s, 0, i - 1);
+			s2 := Copy(s, i + 1, Length(s)-(i-1));
+			calcul := power(calcul(s1),calcul(s2));
+			exit;
+			end
+         else
+		i:=i-1;
+         end;
+        
+        i := Length(s);
 		while i > 0 do
 		begin
 		 if (s[i] = 'c') or (s[i] = 's') then
 		 begin
-			s2 := Copy(s, i + 1, Length(s));
+			s2 := Copy(s, i + 1, Length(s)-(i-1));
 			if s[i] = 'c' then
 			calcul := cos(DegToRad(calcul(s2)))
 			else
@@ -138,3 +180,6 @@ var j:Integer;
 
 END.
 
+/// exposant  +
+///square root +
+///Histoirique
--- test3.pas
+++ test3.pas
@@ -1,18 +1,20 @@
 program test3;
-
-uses calculsimple;
-
-var  expressions,check: String;
+uses calculsimple,sysutils;
+const MAX=60;
+var  check: String;
 	 j:Integer; 
 	 result:Real;
+	 SaveOperation: Array [1..Max] of String;
+	 SaveResult:  Array [1..Max] of String;
 begin
 writeln('Bienvenue dans la section calcul classique');
 rules();
 j := 1;
 repeat
-	lireChaine(expressions);
-	result := calcul(expressions);
-	Writeln(result);
+	lireChaine(SaveOperation[j]);
+	result := calcul(SaveOperation[j]);
+	SaveResult[j]:=FloatToStr(result);
+	Writeln(SaveResult[j]);
 	repeat
 		writeln(' continue /c or exist /e ');
 		readln(check);
