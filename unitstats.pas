245970d2d4c31bff4c5bf822116847f85a193189
Amadouug <150296638+Amadouug@users.noreply.github.com>
2023-12-15 15:09:51 +0100

    Update unitstats.pas

--- unitstats.pas
+++ unitstats.pas
@@ -7,7 +7,10 @@ const MAX=20;
 type t = Array [0..MAX] of Real ;
 type tableau1D=Array of Real;
 type subtab=array of Array of real;
+Type tableau = Array of Array of String;
 
+procedure stats();
+procedure tab_real_to_str(var t: tableau;var TabI: subtab);
 procedure SaisirTableau(var TabI:subtab ; var n:integer);
 procedure creationtableau(var tab_med: tableau1D;const TabI: subtab) ;
 function CalculerMoyenne(tab_med:Tableau1D):real;
@@ -17,7 +20,93 @@ function CalculerEcartType(CalculerVariance: Real): Real;
 
 Implementation
 
-uses SysUtils, Math;
+uses SysUtils, Math, unitdim;
+
+procedure stats();
+var
+  option: char;
+  nm: string;
+  n: integer;
+  TabI: subtab;
+  tab_med: tableau1D;
+  tableauSaisi: boolean;
+  t:tableau;
+begin
+  tableauSaisi := False;
+
+  repeat
+    writeln('Menu de calcul statistique:');
+    writeln('0. Saisir un tableau depuis un fichier');
+    writeln('1. Saisir un tableau');
+    writeln('2. Afficher la moyenne');
+    writeln('3. Afficher la mediane');
+    writeln('4. Afficher la variance');
+    writeln('5. Afficher l''ecart type');
+    writeln('Q. Quitter');
+    writeln;
+
+    write('Choisissez une option : ');
+    readln(option);
+
+    case option of
+      '0': begin
+		 chargementdonnees(t,nm,'stat');
+		 tab_real_to_str(t,TabI);
+		 creationtableau(tab_med, TabI);
+		 tableauSaisi := True;
+		end;
+      '1': begin
+             SaisirTableau(TabI, n);
+             creationtableau(tab_med, TabI);
+             tableauSaisi := True;
+           end;
+      '2': begin
+             if tableauSaisi then
+               writeln('La moyenne est : ',FloatToStr(CalculerMoyenne(tab_med)))
+             else
+               writeln('Veuillez d''abord saisir le tableau.');
+           end;
+      '3': begin
+             if tableauSaisi then
+               writeln('La mediane est : ', FloatToStr(CalculerMediane(tab_med)))
+             else
+               writeln('Veuillez d''abord saisir le tableau.');
+           end;
+      '4': begin
+             if tableauSaisi then
+               writeln('La variance est : ', FloatToStr(CalculerVariance(tab_med)))
+             else
+               writeln('Veuillez d''abord saisir le tableau.');
+           end;
+      '5': begin
+             if tableauSaisi then
+               writeln('L''ecart type est : ', FloatToStr(CalculerEcartType(CalculerVariance(tab_med))))
+             else
+               writeln('Veuillez d''abord saisir le tableau.');
+           end;
+      'Q', 'q': writeln('Au revoir !');
+    else
+      writeln('Option invalide. Veuillez choisir une option valide.');
+    end;
+
+    writeln; 
+  until (option = 'Q') or (option = 'q');
+end;
+
+procedure tab_real_to_str(var t: tableau;var TabI: subtab);
+var i, j: integer;
+	nm: string;
+	currentfile: TextFile;
+begin
+nm:=parcoursfichier(currentfile,'stat');
+for i:=0 to (StrToInt(nm)-StrToInt(nm) mod 10) div 10 do
+	begin
+	for j:=0 to StrToInt(nm) mod 10 do
+		begin
+		t[i][j]:=FloatToStr(TabI[i][j]);
+		end;
+	end;
+end;
 
 procedure SaisirTableau(var TabI:subtab ; var n:integer);
 var i, effectif:integer;
