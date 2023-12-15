c4fb2e5f4b91c457250c0e4ef37dfda9e9a871ba
SARRAU-Arthur <146205377+SARRAU-Arthur@users.noreply.github.com>
2023-12-15 14:24:34 +0100

    Add files via upload

--- unitdim.pas
+++ unitdim.pas
@@ -6,29 +6,21 @@ Const cheminacces='C:\Users\sarra\OneDrive\Documents\INSA\Projet Informatique\';
 
 Type tableau = Array of Array of String;
 	
-Type EStab = Array [1..3,1..2] of String;
-
 Type constantes = Record
-	constante, valeur, unite: string
+	constante, valeur, unite, dimension: string
 	end;
 	
 Type conversions = Record
 	unite_i, unite_f, valeur_i: string
 	end;
-	
-Type dimensions = Record
-	grandeur: string
-	end;
 
-Type ordres_grandeurs = Record
-	prefixe, puissance_de_10: string
-	end;
+{Type dimensions = Record
+	grandeur: string
+	end;}
 
 Type fichiers = Record
 	cv: conversions;
-	ct: constantes;
-	dm: dimensions;
-	og: ordres_grandeurs;
+	ct: constantes
 	end;
 
 procedure fonctionnalites(var f: fichiers);
@@ -37,44 +29,65 @@ procedure chargementdonnees(var t:tableau; var nm: string; nom: string);
 function conversion(var currentfile: TextFile): Extended;
 function constante(var currentfile: TextFile): string;
 //function dimension (var currentfile: TextFile): string;
-procedure ajout(var f: fichiers; var t: tableau);
+procedure ajout(var f: fichiers);
 
 Implementation
 
-uses Sysutils, Math;
+uses Sysutils, Math, Crt;
 
+// Programme principale de l'unite
 procedure fonctionnalites(var f: fichiers);
 var valide: Boolean; 
 	choix: string; 
 	currentfile: TextFile;
-	t: tableau;
 begin
 valide:=True;
 choix:='';
 repeat
 	begin
+	ClrScr();
 	writeln('Que souhaitez-vous realiser ?'); 
-	writeln('Entrez "cv" pour effectuer une conversion d"unites');
-	writeln('Entrez "ct" pour connaitre la valeur d"une constante');
-	writeln('Entrez "dm" pour verifier la dimension d"une grandeur');
-	writeln('Entrez "aj" pour ajouter des donnees dans la memoire de la calculatrice');
-	writeln('Entrez "cs" pour consulter les consignes d"utilisation');
+	writeln('Entrez "cv" pour une conversions');
+	writeln('Entrez "ct" pour la valeur de constante');
+	//writeln('Entrez "dm" pour la dimension');
+	writeln('Entrez "aj" pour ajouter des donnees');
+	write('Choix: ');
 	readln(choix);
-	case choix of
-		'cv': conversion(currentfile);
-		'ct': constante(currentfile);
-		//'dm': dimension(currentfile);
-		'aj': ajout(f,t);
-	else
+	ClrScr();
+	if choix='cv' then	
+		conversion(currentfile);
+	if choix='ct' then	
+		constante(currentfile);
+	if choix='aj' then	
+		ajout(f);
+	if choix='exit' then
+		exit;
+	{if choix='dm' then	
+		dimension(currentfile);}
+	if (choix<>'cv') and (choix<>'ct') and (choix<>'aj') then
 		begin
-		writeln('Saisie incorrecte, veuillez recommencer');
+		writeln('Saisie incorrecte');
 		valide:=False;
 		end;
-	end;
+	writeln('Souhaitez-vous continuer ? "o" / "n" : ');
+	choix:=ReadKey;
+	case choix of
+		#111: 
+		begin
+		valide:=False;
+		end;
+		#110: 
+		begin
+		valide:=True;
+		ClrScr();
+		exit;
+		end;
+		end;
 	end;
 	until valide;
 end;
 
+// Determination nombre de lignes et colonnes d'un fichier csv ";"
 function parcoursfichier(var currentfile: TextFile; nom: String): String;
 var k, n, m: Integer;
 	lines: String;
@@ -96,6 +109,7 @@ for k:=1 to length(lines) do
 parcoursfichier:=IntToStr(n)+IntToStr(m);
 end;
 
+// Chargement fichier csv en ";" vers un tableau Pascal
 procedure chargementdonnees(var t: tableau; var nm: string; nom: string);
 var i, j, k: Integer;
 	lines, currentsquare, affichage: String;
@@ -130,19 +144,27 @@ while not(eof(currentfile)) do
 close(currentfile);
 end;
 
+// Realisation de conversions entre differentes unites et ordres de grandeur
 function conversion(var currentfile: TextFile): Extended;
 var i, j, k, i_c, i_l, f_c, f_l: Integer;
 	cv: conversions;
-	trouve_i, trouve_f: Boolean;
-	nm, og_nm, unite_i, unite_f: String;
+	trouve_i, trouve_f, autorise: Boolean;
+	nm, og_nm, unite_i, unite_f, valide: String;
 	t, og: tableau;
 	result, result_int, power_adjust: Extended;
 begin
+// Chargement fichier vers tableau avec donnees de conversions
 chargementdonnees(t,nm,'conversions');
-chargementdonnees(og,og_nm,'ordres_grandeurs');
+// Chargement fichier vers tableau avec donnes d'ordres de grandeur
+chargementdonnees(og,og_nm,'ordres grandeurs');
 trouve_i:=False;
 trouve_f:=False;
+autorise:=True;
 power_adjust:=1E0;
+conversion:=1E0;
+cv.unite_i:='';
+cv.unite_f:='';
+valide:='0123456789E+-,';
 // Saisie de valeur initiale et unites en jeu dans la conversion
 write('Valeur intiale: ');
 readln(cv.valeur_i);
@@ -189,8 +211,13 @@ while (i<=(StrToInt(nm)-(StrToInt(nm) mod 10)) div 10-1) and (not(trouve_i) or n
 	i+=1;
 	end;
 // Calcul du resultat de conversion sans prise en compte des ordres de grandeurs eventuels
-if not(trouve_i) and not(trouve_f) then
-	writeln('Impossible de realiser votre conversion, verifiez les unites et formats de votre saisie')
+for i:=1 to length(cv.valeur_i) do	
+	begin
+	if (pos(cv.valeur_i[i],valide)=0) or (cv.unite_f='') or (cv.unite_i='') then
+		autorise:=False;
+	end;
+if (not(trouve_i) and not(trouve_f)) or not(autorise) then
+	writeln('Erreur, verifiez les unites et formats de votre saisie')
 else
 	begin
 	if cv.unite_i<>cv.unite_f then 
@@ -206,6 +233,7 @@ else
 	// Integration au resultat des eventuels ordres de grandeurs
 	if (cv.unite_i<>unite_i) or (cv.unite_f<>unite_f) then
 		begin
+		writeln('OK');
 		for k:=1 to (StrToInt(og_nm)-(StrToInt(og_nm) mod 10)) div 10 - 1 do
 			begin
 			if (cv.unite_i<>cv.unite_f) then
@@ -224,43 +252,50 @@ else
 					power_adjust/=StrToFloat(og[k][1]);	
 				end;
 			end;
-		//writeln(power_adjust);
+		writeln(power_adjust);
 		result*=power_adjust;
 		end;
-	end;
 	conversion:=result;
-	// Affichage a l'ecran
+	// Affichage a l'ecran, potentiellement ameliorable avec modulo et div, pour n'afficher que "precision" chiffres a l'ecran
+	ClrScr();
 	writeln(cv.valeur_i,' ',unite_i,' ','=',' ',conversion,' ',unite_f);
+	end;
 end;
 
+// Consultation valeur d'une constante physique
 function constante(var currentfile: TextFile): string;
-var constant, nm, valeur, unite: string;
-	i: integer;
+var nm: string;
+	ct: constantes;
+	i: Integer;
 	t: tableau;
 	trouve: Boolean;
 begin
 chargementdonnees(t,nm,'constantes');
-writeln('De quelle constante souhaitez-vous consulter la valeur');
-readln(constant);
+write('Constante dont vous souhaitez consulter la valeur: ');
+readln(ct.constante);
 trouve:=False;
-i:=-1;
+i:=0;
 while (i<=(StrToInt(nm)-(StrToInt(nm) mod 10)) div 10) and not(trouve) do
 	begin
-	if (t[i][0]=constant) or (t[i][1]=constant) then
+	if (t[i][0]=ct.constante) or (t[i][1]=ct.constante) then
 		begin
-		valeur:=t[i][2];
-		unite:=t[i][3];
-		constant:=t[i][0];
+		ct.valeur:=t[i][2];
+		ct.unite:=t[i][3];
+		ct.constante:=t[i][0];
+		ct.dimension:=t[i][4];
 		trouve:=True;
 		end;
+	i+=1;
 	end;
 if trouve=False then
 	writeln('Saisie incorrecte, veuillez recommencer');
-constante:=constant + ' = ' + valeur + ' ' + unite;
+constante:=ct.constante + ' = ' + ct.valeur + ' ' + ct.unite + ' ; dimension = ' + ct.dimension ;
+ClrScr();
 writeln(constante);
 end;
 
-{function dimension (var currentfile: TextFile): string;
+{Determination de la dimension d'une operations de grandeurs
+function dimension (var currentfile: TextFile): string;
 var nm: string;
 	t: tableau;
 	decoupage: Array of 
@@ -280,25 +315,27 @@ for i=1 to length(grandeurs) do
 dimension:='à coder grâce aux CODES ASCII';
 end;}
 
-procedure ajout(var f: fichiers; var t: tableau);
+// Ajout d'informations dans les fichiers de base de données
+procedure ajout(var f: fichiers);
 var nom_fichier_ajout, nm, saisie: String;
 	k: Integer;
 	fichierajout: TextFile;
+	t: tableau;
 begin
 writeln('Quel fichier souhaitez-vous editer ?');
-write('Saisissez au choix: "conversions" / "constantes" / "dimensions" / "odres_grandeur": ');
+write('Saisissez au choix: "conversions" / "constantes" / "dimensions" / "odres grandeur": ');
 readln(nom_fichier_ajout);
+chargementdonnees(t,nm,nom_fichier_ajout);
 assign(fichierajout,cheminacces + nom_fichier_ajout + '.csv');
 append(fichierajout);
-chargementdonnees(t,nm,nom_fichier_ajout);
-for k:=1 to StrToInt(nm[2]) do
+writeln(fichierajout,'');
+for k:=0 to (StrToInt(nm) mod 10) - 1 do
 	begin
-	writeln('Saisissez',' ',t[1][k]);
+	write('Saisissez',' ',t[0][k] + ': ');
 	readln(saisie); 
-	writeln(saisie);
-	setlength(t,StrToInt(nm[1])+1,StrToInt(nm[2]));
-	t[StrToInt(nm[1])+1][k]:=saisie+';'
+	write(fichierajout,saisie + ';');
 	end;
+ClrScr();
 close(fichierajout);
 end;
 
