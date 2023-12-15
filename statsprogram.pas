2f73d6dc99852bc3bf3a0ea814130c858f29d672
Amadouug <150296638+Amadouug@users.noreply.github.com>
2023-12-15 15:10:41 +0100

    Update statsprogram.pas

--- statsprogram.pas
+++ statsprogram.pas
@@ -1,66 +1,10 @@
 program StatsProgram;
 
 uses
-  unitstats,sysutils;
-
-var
-  option: char;
-  n: integer;
-  TabI: subtab;
-  tab_med: tableau1D;
-  tableauSaisi: boolean;
-
+  unitstats;
+  
 begin
-  tableauSaisi := False;
-
-  repeat
-    writeln('Menu de calcul statistique:');
-    writeln('1. Saisir un tableau');
-    writeln('2. Afficher la moyenne');
-    writeln('3. Afficher la mediane');
-    writeln('4. Afficher la variance');
-    writeln('5. Afficher l''ecart type');
-    writeln('Q. Quitter');
-    writeln;
-
-    write('Choisissez une option : ');
-    readln(option);
-
-    case option of
-      '1': begin
-             SaisirTableau(TabI, n);
-             creationtableau(tab_med, TabI);
-             tableauSaisi := True;
-           end;
-      '2': begin
-             if tableauSaisi then
-               writeln('La moyenne est : ',FloatToStr(CalculerMoyenne(tab_med)))
-             else
-               writeln('Veuillez d''abord saisir le tableau.');
-           end;
-      '3': begin
-             if tableauSaisi then
-               writeln('La mediane est : ', FloatToStr(CalculerMediane(tab_med)))
-             else
-               writeln('Veuillez d''abord saisir le tableau.');
-           end;
-      '4': begin
-             if tableauSaisi then
-               writeln('La variance est : ', FloatToStr(CalculerVariance(tab_med)))
-             else
-               writeln('Veuillez d''abord saisir le tableau.');
-           end;
-      '5': begin
-             if tableauSaisi then
-               writeln('L''ecart type est : ', FloatToStr(CalculerEcartType(CalculerVariance(tab_med))))
-             else
-               writeln('Veuillez d''abord saisir le tableau.');
-           end;
-      'Q', 'q': writeln('Au revoir !');
-    else
-      writeln('Option invalide. Veuillez choisir une option valide.');
-    end;
-
-    writeln; 
-  until (option = 'Q') or (option = 'q');
+stats;
 end.
+
+
