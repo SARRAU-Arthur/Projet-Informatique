3295898c6201435f7e65893da45c89c05a734e1f
egefisgin <38686980+egefisgin@users.noreply.github.com>
2023-12-15 19:30:49 +0100

    final

--- calculsimple.pas
+++ calculsimple.pas
@@ -175,11 +175,31 @@ var j:Integer;
   calcul := StrToFloat(s);
 	
   end;
- 
-   
+  
+procedure affichage();
+const MAX=60;
+var  check: String;
+	 j:Integer; 
+	 result:Real;
+	 SaveOperation: Array [1..Max] of String;
+	 SaveResult:  Array [1..Max] of String;	
+begin
+	writeln('Bienvenue dans la section calcul classique');
+	rules();
+	j := 1;
+	repeat
+		lireChaine(SaveOperation[j]);
+		result := calcul(SaveOperation[j]);
+		SaveResult[j]:=FloatToStr(result);
+		Writeln(SaveResult[j]);
+		repeat
+			writeln(' continue /c or exist /e ');
+			readln(check);
+			until (check = 'c') or (check = 'e');
+			j := j + 1;
+		until (check = 'e');
+	end;
 
 END.
 
-/// exposant  +
-///square root +
 ///Histoirique
